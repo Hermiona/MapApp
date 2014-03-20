43c2357fab39c5e6cc54e7b6dc4b4c10ddf29623
y.tilekbekov <=>
2014-03-20 12:49:01 +0600

    Отрефакторил названия переменных ресурсов стринг вывел некоторые свойства элементов в файлы ресурсов настроил вывод типа места по гендеру объекта

--- src/com/example/MapApp/ListActivity.java
+++ src/com/example/MapApp/ListActivity.java
@@ -7,6 +7,7 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import com.example.MapApp.Main.XmlReader;
+import com.example.MapApp.PrayerPlace.Gender;
 import com.example.MapApp.PrayerPlace.PrayerPlace;
 
 import java.util.ArrayList;
@@ -36,24 +37,24 @@ public class ListActivity extends Activity {
         host.setup();
 
         TabHost.TabSpec allPrayerPlacesTab = host.newTabSpec("allPrayerPlacesTab");
-        allPrayerPlacesTab.setIndicator(getResources().getString(R.string.all_places),
+        allPrayerPlacesTab.setIndicator(getResources().getString(R.string.list_all_places),
                 getResources().getDrawable(android.R.drawable.star_on));
         allPrayerPlacesTab.setContent(R.id.ScrollViewAllPlaces);
         host.addTab(allPrayerPlacesTab);
 
         TabHost.TabSpec malePrayerPlacesTab = host.newTabSpec("malePrayerPlacesTab");
-        malePrayerPlacesTab.setIndicator(getResources().getString(R.string.male_places),
+        malePrayerPlacesTab.setIndicator(getResources().getString(R.string.list_male_places_text),
                 getResources().getDrawable(android.R.drawable.star_on));
         malePrayerPlacesTab.setContent(R.id.ScrollViewMalePlaces);
         host.addTab(malePrayerPlacesTab);
 
         TabHost.TabSpec femalePrayerPlacesTab = host.newTabSpec("femalePrayerPlacesTab");
-        femalePrayerPlacesTab.setIndicator(getResources().getString(R.string.female_places),
+        femalePrayerPlacesTab.setIndicator(getResources().getString(R.string.list_female_places_text),
                 getResources().getDrawable(android.R.drawable.star_on));
         femalePrayerPlacesTab.setContent(R.id.ScrollViewFemalePlaces);
         host.addTab(femalePrayerPlacesTab);
 
-        host.setCurrentTabByTag("allTab");
+        host.setCurrentTabByTag("allPrayerPlacesTab");
 
         TableLayout allPlacesTable = (TableLayout) findViewById(R.id.TableLayout_AllPlaces);
         TableLayout malePlacesTable = (TableLayout) findViewById(R.id.TableLayout_MalePlaces);
@@ -83,33 +84,50 @@ public class ListActivity extends Activity {
     }
 
     private void initializeHeaderRow(TableLayout placesTable) {
-        // Create the Table header row
         TableRow headerRow = new TableRow(this);
-        int textColor = getResources().getColor(R.color.logo_color);
-        float textSize = getResources().getDimension(R.dimen.help_text_size);
-        addTextToRowWithValues(headerRow, getResources().getString(R.string.name), textColor, textSize);
-        addTextToRowWithValues(headerRow, getResources().getString(R.string.address), textColor, textSize);
-        addTextToRowWithValues(headerRow, getResources().getString(R.string.type), textColor, textSize);
+        int textColor = getResources().getColor(R.color.list_header_color);
+        float textSize = getResources().getDimension(R.dimen.list_header_size);
+        addTextToRowWithValues(headerRow, getResources().getString(R.string.list_place_name), textColor, textSize);
+        addTextToRowWithValues(headerRow, getResources().getString(R.string.list_place_address), textColor, textSize);
+        addTextToRowWithValues(headerRow, getResources().getString(R.string.list_place_type), textColor, textSize);
         placesTable.addView(headerRow);
     }
 
-    private void insertPlaceRow(final TableLayout scoreTable, PrayerPlace prayerPlace) {
-        final TableRow newRow = new TableRow(ListActivity.this);
+    private void insertPlaceRow(TableLayout placesTable, PrayerPlace prayerPlace) {
+        TableRow newRow = new TableRow(ListActivity.this);
 
-        int textColor = getResources().getColor(R.color.title_color);
-        float textSize = getResources().getDimension(R.dimen.help_text_size);
+        int textColor = getResources().getColor(R.color.list_item_color);
+        float textSize = getResources().getDimension(R.dimen.list_item_size);
 
         addTextToRowWithValues(newRow, prayerPlace.name, textColor, textSize);
         addTextToRowWithValues(newRow, prayerPlace.description, textColor, textSize);
-        addTextToRowWithValues(newRow, prayerPlace.address, textColor, textSize);
-        scoreTable.addView(newRow);
+        addTextToRowWithValues(newRow, getGenderTextForDisplay(prayerPlace.prayerPlaceGender), textColor, textSize);
+        placesTable.addView(newRow);
     }
 
     private void addTextToRowWithValues(final TableRow tableRow, String text, int textColor, float textSize) {
+        int rowWidth = tableRow.getWidth();
         TextView textView = new TextView(this);
         textView.setTextSize(textSize);
         textView.setTextColor(textColor);
         textView.setText(text);
+        textView.setWidth(rowWidth);
         tableRow.addView(textView);
+    }
+
+    String getGenderTextForDisplay(Gender gender){
+        String genderText = "";
+        switch (gender){
+            case MALE:
+                genderText = getText(R.string.list_gender_male_text).toString();
+                break;
+            case FEMALE:
+                genderText = getText(R.string.list_gender_female_text).toString();
+                break;
+            case JOINT:
+                genderText = getText(R.string.list_gender_joint_text).toString();
+                break;
+        }
+        return genderText;
     }
 }
